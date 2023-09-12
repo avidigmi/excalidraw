19d991b4505ab22dfd5089b31f8f2280ecf8a106
Avi Digmi <avdigmi@microsoft.com>
2023-09-12 12:54:10 +0300

    fix merge from remote repo

--- excalidraw-app/components/AppWelcomeScreen.tsx
+++ excalidraw-app/components/AppWelcomeScreen.tsx
@@ -1,9 +1,8 @@
 import React from "react";
-import { PlusPromoIcon } from "../../packages/excalidraw/components/icons";
-import { useI18n } from "../../packages/excalidraw/i18n";
+import { PlusPromoIcon } from "../../components/icons";
+import { useI18n } from "../../i18n";
 import { WelcomeScreen } from "../../packages/excalidraw/index";
 import { isExcalidrawPlusSignedUser } from "../app_constants";
-import { POINTER_EVENTS } from "../../packages/excalidraw/constants";
 
 export const AppWelcomeScreen: React.FC<{
   setCollabDialogShown: (toggle: boolean) => any;
@@ -18,15 +17,7 @@ export const AppWelcomeScreen: React.FC<{
       .map((bit, idx) => {
         if (bit === "Excalidraw+") {
           return (
-            <a
-              style={{ pointerEvents: POINTER_EVENTS.inheritFromUI }}
-              href={`${
-                import.meta.env.VITE_APP_PLUS_APP
-              }?utm_source=excalidraw&utm_medium=app&utm_content=welcomeScreenSignedInUser`}
-              key={idx}
-            >
-              Excalidraw+
-            </a>
+            <div/>
           );
         }
         return bit;
@@ -50,13 +41,6 @@ export const AppWelcomeScreen: React.FC<{
         <WelcomeScreen.Center.Menu>
           <WelcomeScreen.Center.MenuItemLoadScene />
           <WelcomeScreen.Center.MenuItemHelp />
-          {props.isCollabEnabled && (
-            <WelcomeScreen.Center.MenuItemLiveCollaborationTrigger
-              onSelect={() => props.setCollabDialogShown(true)}
-            />
-          )}
-                import.meta.env.VITE_APP_PLUS_LP
-              }/plus?utm_source=excalidraw&utm_medium=app&utm_content=welcomeScreenGuest`}
         </WelcomeScreen.Center.Menu>
       </WelcomeScreen.Center>
     </WelcomeScreen>
